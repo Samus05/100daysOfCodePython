c6ab6c806286f5a68367c2c46d076cdd00aa3307
Samus05 <samguerrerolozano@gmail.com>
2024-01-04 18:57:45 -0600

    correccion programa propinas

--- Day-02-CalculadoraPropinas/main.py
+++ Day-02-CalculadoraPropinas/main.py
@@ -3,6 +3,6 @@ total = float(input("Cual es el total de tu cuenta"))
 porcentajePropina = int(input("Cual es el porcentaje que deseas dejar de propina? 10% 12% o 15% ?"))
 personas = int(input("Con cuantas personas dividiras la cuenta?"))
 
-propina = (total / personas)*(porcentajePropina*0.01)
+propina = round((total / personas)*(porcentajePropina*0.01),2)
 
-print(f"cada persona debe pagar {propina}")
+print(f"cada persona debe pagar ${propina}")
